b8655dd2da605dc88c8a3f46e9722a1544932812
sharreysuhendra22 <49983@jisedu.or.id>
2020-04-29 17:20:32 +0700

    Created countdown

---
+++ Game - Jam 3/Assets/Scripts/Countdown.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Countdown : MonoBehaviour {
+    public float timeStart = 60;
+    public Text textBox;
+
+	// Use this for initialization
+	void Start () {
+        textBox.text = timeStart.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(timeStart <= 0){
+            timeStart = 0.0f;
+            textBox.text = "Game Over";
+        }
+        else{
+            timeStart -= Time.deltaTime;
+            textBox.text = Mathf.Round(timeStart).ToString();
+        }
+	}
+}
