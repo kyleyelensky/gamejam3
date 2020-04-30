eb6c57f14a02deb97ee37cb3e3c4ff484e679a89
sharreysuhendra22 <49983@jisedu.or.id>
2020-04-30 15:25:39 +0700

    added moving butterfly

---
+++ Game - Jam 3/Assets/Scripts/MoveButterfly.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveButterfly : MonoBehaviour
+{
+    void Update(){
+       transform.Translate(0f, Input.GetAxis("Vertical")*10f*Time.deltaTime, 0f); 
+       transform.Translate(Input.GetAxis("Horizontal")*10f*Time.deltaTime, 0f, 0f); 
+    }
+}
